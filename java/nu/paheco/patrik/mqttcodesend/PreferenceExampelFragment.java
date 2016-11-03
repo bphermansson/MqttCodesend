53a6476620a1b2c9ae8dc6df7751ad4f62b65c15
Patrik <patrik@paheco.nu>
2016-11-03 07:15:53 +0100

    Updated settings

--- java/nu/paheco/patrik/mqttcodesend/PreferenceExampelFragment.java
+++ java/nu/paheco/patrik/mqttcodesend/PreferenceExampelFragment.java
@@ -33,29 +33,39 @@ public class PreferenceExampelFragment extends PreferenceFragment {
         public void onCreate(final Bundle savedInstanceState)
         {
             super.onCreate(savedInstanceState);
-            addPreferencesFromResource(R.xml.preferences);
+            //addPreferencesFromResource(R.xml.preferences);
 
 
         }
         @Override
         public void onResume() {
             super.onResume();
-
-
             addPreferencesFromResource(R.xml.preferences);
 
             // Find preferences and set their summaries to the current value
+            // Main screen
             String mqttip = PreferenceManager.getDefaultSharedPreferences(getActivity()).getString("mqttip", "");
             String mqttuser = PreferenceManager.getDefaultSharedPreferences(getActivity()).getString("mqttuser", "");
             String mqttpass = PreferenceManager.getDefaultSharedPreferences(getActivity()).getString("mqttpass", "");
 
+            // Button screen
+            String pref_config_1 = PreferenceManager.getDefaultSharedPreferences(getActivity()).getString("pref_config_1", "");
+
+            // Main screen
             EditTextPreference Prefmqttip = (EditTextPreference)findPreference("mqttip");
             EditTextPreference Prefmqttuser = (EditTextPreference)findPreference("mqttuser");
             EditTextPreference Prefmqttpass = (EditTextPreference)findPreference("mqttpass");
 
+            // Button screen
+            EditTextPreference PrefC1 = (EditTextPreference)findPreference("pref_config_1");
+
+            // Main screen
             Prefmqttip.setSummary(mqttip);
             Prefmqttuser.setSummary(mqttuser);
             Prefmqttpass.setSummary(mqttpass);
+
+            // Button screen
+            PrefC1.setSummary(pref_config_1);
 
             new SharedPreferences.OnSharedPreferenceChangeListener()
             {
